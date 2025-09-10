59ec73a44f1202e65fe2bc3fe2df8e6cb19a55f5
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-10 19:47:22 +0530

    Code of September

--- src/TenSeptember.java
+++ src/TenSeptember.java
@@ -1,4 +1,6 @@
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 public class TenSeptember {
     public static int[] TwoSum(int[] nums, int target) {
@@ -12,10 +14,26 @@ public class TenSeptember {
         return new int[]{-1, -1};
     }
 
+    public static int[] TwoSumOptimal(int[] nums, int target){
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for(int i=0; i<nums.length; i++){
+            int requiredNo = target - nums[i];
+
+            if(map.containsKey(requiredNo)){
+                return new int[]{i, map.get(requiredNo)};
+            }
+
+            map.put(nums[i], i);
+        }
+        return null;
+    }
+
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 5, 4, 8, 6, 4};
 
         System.out.println(Arrays.toString(TwoSum(nums, 8)));
+        System.out.println(Arrays.toString(TwoSumOptimal(nums, 45)));
 
     }
 }
