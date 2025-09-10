da67de0cd1a6000a6e7f9eca195fc8cbc9bd6e66
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-10 19:31:14 +0530

    Code of September

--- src/TenSeptember.java
+++ src/TenSeptember.java
@@ -1,6 +1,21 @@
-public class TenSeptember {
-    public static void main(String[] args) {
+import java.util.Arrays;
 
+public class TenSeptember {
+    public static int[] TwoSum(int[] nums, int target) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[]{i, j};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 5, 4, 8, 6, 4};
+
+        System.out.println(Arrays.toString(TwoSum(nums, 8)));
 
     }
 }
