ad0b75984c01e79b092500432f7c40f1c3cde659
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-11 17:33:04 +0530

    Code of September

--- src/ElevenSeptember.java
+++ src/ElevenSeptember.java
@@ -1,27 +1,27 @@
 import java.util.Arrays;
 
 public class ElevenSeptember {
-    public static int removeDuplicates(int[] nums){
+    public static int removeDuplicates(int[] nums) {
+        Arrays.sort(nums); // ensure sorted
         int k = 1;
-        for(int i=1; i<nums.length; i++){
-            if(nums[i] != nums[i-1]){
+        for (int i = 1; i < nums.length; i++) {
+            if (nums[i] != nums[i - 1]) {
                 nums[k] = nums[i];
                 k++;
-
             }
         }
         return k;
     }
 
-    public static void rotateArray(int[] nums, int k){
+    public static void rotateArray(int[] nums, int k) {
         k = k % nums.length;
-        revNumber(nums, 0, nums.length-1);
-        revNumber(nums, 0, k-1);
-        revNumber(nums, k, nums.length-1);
+        revNumber(nums, 0, nums.length - 1);
+        revNumber(nums, 0, k - 1);
+        revNumber(nums, k, nums.length - 1);
     }
 
-    public static void revNumber(int[] nums, int start, int end){
-        while (start <= end){
+    public static void revNumber(int[] nums, int start, int end) {
+        while (start < end) { // fixed
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
@@ -29,13 +29,16 @@ public class ElevenSeptember {
             end--;
         }
     }
+
     public static void main(String[] args) {
-        int[] nums = {1,2,3,4,4,5,7,5,8};
+        int[] nums = {1, 2, 3, 4, 4, 5, 7, 5, 8};
 
-        System.out.println(removeDuplicates(nums));
+        int k = removeDuplicates(nums);
+        System.out.println("Unique count: " + k);
 
-        rotateArray(nums, 4);
-
-        System.out.println(Arrays.toString(nums));
+        int[] unique = Arrays.copyOf(nums, k);  // only unique elements
+        rotateArray(unique, 4);
+        System.out.println("Rotated array: " + Arrays.toString(unique));
     }
+
 }
