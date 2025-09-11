1794141e37f584956b3d16b89b5262acd9d6bf81
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-11 17:28:51 +0530

    Code of September

--- src/ElevenSeptember.java
+++ src/ElevenSeptember.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class ElevenSeptember {
     public static int removeDuplicates(int[] nums){
         int k = 1;
@@ -10,9 +12,30 @@ public class ElevenSeptember {
         }
         return k;
     }
+
+    public static void rotateArray(int[] nums, int k){
+        k = k % nums.length;
+        revNumber(nums, 0, nums.length-1);
+        revNumber(nums, 0, k-1);
+        revNumber(nums, k, nums.length-1);
+    }
+
+    public static void revNumber(int[] nums, int start, int end){
+        while (start <= end){
+            int temp = nums[start];
+            nums[start] = nums[end];
+            nums[end] = temp;
+            start++;
+            end--;
+        }
+    }
     public static void main(String[] args) {
         int[] nums = {1,2,3,4,4,5,7,5,8};
 
         System.out.println(removeDuplicates(nums));
+
+        rotateArray(nums, 4);
+
+        System.out.println(Arrays.toString(nums));
     }
 }
