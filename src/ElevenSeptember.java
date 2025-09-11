bc3273d433015d749094e5247c4f5e5f2c241ebd
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-11 17:48:09 +0530

    Code of September

--- src/ElevenSeptember.java
+++ src/ElevenSeptember.java
@@ -1,8 +1,10 @@
 import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 
 public class ElevenSeptember {
     public static int removeDuplicates(int[] nums) {
-        Arrays.sort(nums); // ensure sorted
+        Arrays.sort(nums);
         int k = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i] != nums[i - 1]) {
@@ -21,7 +23,7 @@ public class ElevenSeptember {
     }
 
     public static void revNumber(int[] nums, int start, int end) {
-        while (start < end) { // fixed
+        while (start < end) {
             int temp = nums[start];
             nums[start] = nums[end];
             nums[end] = temp;
@@ -30,15 +32,30 @@ public class ElevenSeptember {
         }
     }
 
+    public static boolean containsDuplicate(int[] nums){
+        Set<Integer> dup = new HashSet<>();
+        for(int num: nums){
+            if(dup.contains(num)){
+                return true;
+            }
+            dup.add(num);
+        }
+        return false;
+    }
+
+
+
     public static void main(String[] args) {
         int[] nums = {1, 2, 3, 4, 4, 5, 7, 5, 8};
 
         int k = removeDuplicates(nums);
         System.out.println("Unique count: " + k);
 
-        int[] unique = Arrays.copyOf(nums, k);  // only unique elements
+        int[] unique = Arrays.copyOf(nums, k);
         rotateArray(unique, 4);
         System.out.println("Rotated array: " + Arrays.toString(unique));
+
+        System.out.println(containsDuplicate(nums));
     }
 
 }
