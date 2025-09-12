540b54e4088db196c1e675f6e78e5c4257cae647
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-12 18:10:52 +0530

    Code of September

--- src/TwelveSeptember.java
+++ src/TwelveSeptember.java
@@ -16,10 +16,31 @@ public class TwelveSeptember {
         return result;
     }
 
+    public static void zeroes(int[] nums) {
+        int k = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+            if(nums[i] != 0){
+                nums[k] = nums[i];
+                k++;
+            }
+        }
+        while(k < nums.length){
+            nums[k] = 0;
+            k++;
+        }
+    }
+
 
     public static void main(String[] args) {
         int[] nums = {0};
 
+        int[] arr = {0,1,0,3,12};
+
         System.out.println(Arrays.toString(plusOne(nums)));
+
+        zeroes(arr);
+
+        System.out.println(Arrays.toString(arr));
     }
 }
