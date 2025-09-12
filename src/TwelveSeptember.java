4afda2a87a067f890954e5c19663ababd5ce2614
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-12 17:57:31 +0530

    Code of September

--- src/TwelveSeptember.java
+++ src/TwelveSeptember.java
@@ -2,19 +2,21 @@ import java.util.Arrays;
 
 public class TwelveSeptember {
 
-    public static int[] plusOne(int[] nums){
+    public static int[] plusOne(int[] nums) {
         int[] result = new int[nums.length + 1];
         result[0] = 1;
-        for(int i = nums.length-1; i>0; i--){
-            if(nums[i] == 9){
+        for (int i = nums.length - 1; i > 0; i--) {
+            if (nums[i] == 9) {
                 nums[i] = 0;
-            }else{
+            } else {
                 nums[i] = nums[i] + 1;
                 return nums;
             }
         }
         return result;
     }
+
+
     public static void main(String[] args) {
         int[] nums = {9};
 
