4575e863fd7dc4475a8c957efbe41c8f6196e519
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-13 17:01:23 +0530

    Code of September

--- src/ThirteenSeptember.java
+++ src/ThirteenSeptember.java
@@ -1,5 +1,5 @@
 public class ThirteenSeptember {
-    public static int findPivotIndex(int[] nums){
+    public static int findPivotIndex(int[] nums) {
         int rightSum = 0;
         for (int num : nums) {
             rightSum += num;
@@ -7,8 +7,8 @@ public class ThirteenSeptember {
 
         int leftSum = 0;
 
-        for(int num: nums){
-            if(leftSum == rightSum - num){
+        for (int num : nums) {
+            if (leftSum == rightSum - num) {
                 return num;
             }
 
@@ -17,11 +17,35 @@ public class ThirteenSeptember {
         }
         return -1;
     }
+
+    public static int dominantIndex(int[] nums) {
+        int max = Integer.MIN_VALUE;
+        int index = -1;
+
+        for (int i = 0; i < nums.length; i++) {
+            if (max < nums[i]) {
+                max = nums[i];
+                index = i;
+            }
+        }
+
+        for (int i = 0; i < nums.length; i++) {
+            if (i != index && max < 2 * nums[i]) {
+                return -1;
+            }
+        }
+        return index;
+    }
+
     public static void main(String[] args) {
 
-        int[] nums = {1,7,3,9,6,5,6};
+        int[] nums = {1, 7, 3, 9, 6, 5, 6};
 
+        int[] nums1 = {3, 6, 1, 0};
 
         System.out.println(findPivotIndex(nums));
+
+        System.out.println(dominantIndex(nums1));
+
     }
 }
