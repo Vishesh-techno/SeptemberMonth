2dd9846aa9c5c6e2db7afb7f34f82b52aaebc726
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-14 16:37:20 +0530

    Code of September

---
+++ src/FourteenSeptember.java
@@ -0,0 +1,50 @@
+public class FourteenSeptember {
+
+    static class ListNode{
+        int data;
+        ListNode next;
+
+        public ListNode (int data){
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static ListNode head;
+
+    public static void addNode(int data){
+        ListNode newNode = new ListNode(data);
+
+        if(head == null){
+            head = newNode;
+        }else{
+            ListNode temp = head;
+
+            while(temp.next != null){
+                temp = temp.next;
+            }
+            temp.next = newNode;
+        }
+    }
+
+    public static void print(){
+        ListNode temp = head;
+
+        while(temp.next != null){
+            System.out.print(temp.data + "==>");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+
+        addNode(10);
+        addNode(20);
+        addNode(30);
+        addNode(40);
+
+        print();
+
+    }
+}
