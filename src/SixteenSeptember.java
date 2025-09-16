930a3ad7e16b9b9c37b51918324450a3913faecd
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-16 20:09:09 +0530

    Code of September

--- src/SixteenSeptember.java
+++ src/SixteenSeptember.java
@@ -2,37 +2,40 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 public class SixteenSeptember {
-    static class stack{
+    static class stack {
         static ArrayList<Integer> list = new ArrayList<>();
-        public static boolean isEmpty(){
+
+        public static boolean isEmpty() {
             return list.size() == 0;
         }
-//        push
-        public static void push(int data){
+
+        //        push
+        public static void push(int data) {
             list.add(data);
         }
 
-//        pop
-        public static int pop(){
-            if(isEmpty()){
+        //        pop
+        public static int pop() {
+            if (isEmpty()) {
                 return -1;
             }
 
             int top = list.get(list.size() - 1);
-            list.remove(list.size()-1);
+            list.remove(list.size() - 1);
             return top;
         }
 
 //      peek
 
-        public static int peek(){
-            if(isEmpty()){
+        public static int peek() {
+            if (isEmpty()) {
                 return -1;
             }
 
-            return list.get(list.size()-1);
+            return list.get(list.size() - 1);
         }
     }
+
     public static void main(String[] args) {
 
         Stack s = new Stack<>();
@@ -43,7 +46,7 @@ public class SixteenSeptember {
 
         System.out.println(s);
 
-        while(!s.isEmpty()){
+        while (!s.isEmpty()) {
             System.out.println(s.peek());
             s.pop();
         }
