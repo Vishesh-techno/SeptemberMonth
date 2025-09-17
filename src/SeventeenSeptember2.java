b89420e76596c697e5255dd0639a4c0d192dd395
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-17 16:58:38 +0530

    Code of September

--- src/SeventeenSeptember2.java
+++ src/SeventeenSeptember2.java
@@ -1,9 +1,9 @@
 public class SeventeenSeptember2 {
-    public static void reverseString(char[] ch){
+    public static void reverseString(char[] ch) {
         int start = 0;
         int end = ch.length - 1;
 
-        while(start < end){
+        while (start < end) {
             char temp = ch[start];
             ch[start] = ch[end];
             ch[end] = temp;
@@ -12,11 +12,28 @@ public class SeventeenSeptember2 {
         }
     }
 
+    public static int reverseInt(int x) {
+        long rev = 0;
+        while (x != 0) {
+            int last = x % 10;
+            rev = rev * 10 + last;
+            if (rev < Integer.MIN_VALUE || rev > Integer.MAX_VALUE) {
+                return 0;
+            }
+            x /= 10;
+        }
+        return (int) rev;
+    }
+
     public static void main(String[] args) {
-        char[] s = {'h','e','l','l','o'};
+        char[] s = {'h', 'e', 'l', 'l', 'o'};
+
+        int x = 1534236469;
 
         reverseString(s);
 
         System.out.println(s);
+
+        System.out.println(reverseInt(x));
     }
 }
