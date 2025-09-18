1e59e69a39bf2887dd377e4332af6ea8c9f0cc96
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-18 20:02:28 +0530

    Code of September

---
+++ src/EighteenSeptember.java
@@ -0,0 +1,31 @@
+public class EighteenSeptember {
+
+    public static int strStr(String hayStack, String needle){
+
+        if(hayStack.length() < needle.length()){
+            return -1;
+        }
+
+        for(int i=0; i<hayStack.length(); i++){
+            int j = 0;
+            while (j<needle.length() && (i+j) < hayStack.length()){
+                if(needle.charAt(j) != hayStack.charAt(i+j)){
+                    break;
+                }
+                j++;
+            }
+
+            if(j == needle.length()){
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    public static void main(String[] args) {
+        String hayStack = "Vishesh";
+        String needle = "esh";
+
+        System.out.println(strStr(hayStack, needle));
+    }
+}
