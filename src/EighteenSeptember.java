f4020f643287e19426dea69ef6f739d0b3abba4b
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-18 20:02:44 +0530

    Code of September

--- src/EighteenSeptember.java
+++ src/EighteenSeptember.java
@@ -1,21 +1,21 @@
 public class EighteenSeptember {
 
-    public static int strStr(String hayStack, String needle){
+    public static int strStr(String hayStack, String needle) {
 
-        if(hayStack.length() < needle.length()){
+        if (hayStack.length() < needle.length()) {
             return -1;
         }
 
-        for(int i=0; i<hayStack.length(); i++){
+        for (int i = 0; i < hayStack.length(); i++) {
             int j = 0;
-            while (j<needle.length() && (i+j) < hayStack.length()){
-                if(needle.charAt(j) != hayStack.charAt(i+j)){
+            while (j < needle.length() && (i + j) < hayStack.length()) {
+                if (needle.charAt(j) != hayStack.charAt(i + j)) {
                     break;
                 }
                 j++;
             }
 
-            if(j == needle.length()){
+            if (j == needle.length()) {
                 return i;
             }
         }
