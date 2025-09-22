fc2902d83aba37083e18630d71a7de73f158b0fb
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-22 12:02:42 +0530

    Code of September

--- src/TwentyTwoSeptember.java
+++ src/TwentyTwoSeptember.java
@@ -37,6 +37,33 @@ public class TwentyTwoSeptember {
         System.out.println("null");
     }
 
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
+        ListNode res = new ListNode(0);
+        ListNode pointer = res;
+        int carry = 0;
+        while(l1 != null || l2 != null){
+            int sum = 0 + carry;
+
+            if(l1 != null){
+                sum += l1.data;
+                l1 = l1.next;
+            }
+
+            if(l2 != null){
+                sum += l2.data;
+                l2 = l2.next;
+            }
+
+            carry = sum/10;
+            sum = sum%10;
+            pointer.next = new ListNode(sum);
+            pointer = pointer.next;
+        }
+        if(carry == 1){
+            pointer.next = new ListNode(1);
+        }
+        return res.next;
+    }
 
     
     public static void main(String[] args) {
@@ -50,6 +77,9 @@ public class TwentyTwoSeptember {
         l2.next = new ListNode(4);
         l2.next.next = new ListNode(6);
         print(l2);
+
+        ListNode result = addTwoNumbers(l1, l2);
+        print(result);
         
         
     }
