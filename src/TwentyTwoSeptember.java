13dff97088abb8be75809f7894979cdbaf60b09c
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-22 11:40:55 +0530

    Code of September

---
+++ src/TwentyTwoSeptember.java
@@ -0,0 +1,56 @@
+import java.util.List;
+
+public class TwentyTwoSeptember {
+    static class ListNode{
+        int data;
+        ListNode next;
+
+        public ListNode(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+    public static ListNode head;
+
+    public static ListNode addNode(int data) {
+        ListNode newNode = new ListNode(data);
+
+        if (head == null) {
+            head = newNode;
+            return head;
+        }
+
+        ListNode temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+        return head;
+    }
+
+    public static void print(ListNode head) {
+        ListNode temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + "==>>");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+
+    
+    public static void main(String[] args) {
+        
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(3);
+        l1.next.next = new ListNode(5);
+        print(l1);
+
+       ListNode l2 = new ListNode(2);
+        l2.next = new ListNode(4);
+        l2.next.next = new ListNode(6);
+        print(l2);
+        
+        
+    }
+}
