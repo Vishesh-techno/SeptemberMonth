887b1cc6848ee468aedef04fd41d6b5cf3154797
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-22 12:53:59 +0530

    Code of September

--- src/TwentyTwoSeptember.java
+++ src/TwentyTwoSeptember.java
@@ -1,7 +1,7 @@
 import java.util.List;
 
 public class TwentyTwoSeptember {
-    static class ListNode{
+    static class ListNode {
         int data;
         ListNode next;
 
@@ -10,6 +10,7 @@ public class TwentyTwoSeptember {
             this.next = null;
         }
     }
+
     public static ListNode head;
 
     public static ListNode addNode(int data) {
@@ -37,50 +38,77 @@ public class TwentyTwoSeptember {
         System.out.println("null");
     }
 
-    public static ListNode addTwoNumbers(ListNode l1, ListNode l2){
+    public static ListNode addTwoNumbers(ListNode l1, ListNode l2) {
         ListNode res = new ListNode(0);
         ListNode pointer = res;
         int carry = 0;
-        while(l1 != null || l2 != null){
+        while (l1 != null || l2 != null) {
             int sum = 0 + carry;
 
-            if(l1 != null){
+            if (l1 != null) {
                 sum += l1.data;
                 l1 = l1.next;
             }
 
-            if(l2 != null){
+            if (l2 != null) {
                 sum += l2.data;
                 l2 = l2.next;
             }
 
-            carry = sum/10;
-            sum = sum%10;
+            carry = sum / 10;
+            sum = sum % 10;
             pointer.next = new ListNode(sum);
             pointer = pointer.next;
         }
-        if(carry == 1){
+        if (carry == 1) {
             pointer.next = new ListNode(1);
         }
         return res.next;
     }
 
-    
+    public static ListNode rotateList(ListNode head, int k) {
+        ListNode tail = head;
+        int sz = 1;
+        while (tail.next != null) {
+            tail = tail.next;
+            sz++;
+        }
+
+        k = k % sz;
+        if (k == 0) {
+            return head;
+        }
+
+        int diff = sz - k;
+
+        ListNode curr = head;
+        for (int i = 0; i < diff - 1; i++) {
+            curr = curr.next;
+        }
+
+        ListNode newHead = curr.next;
+        curr.next = null;
+        tail.next = head;
+        return newHead;
+    }
+
+
     public static void main(String[] args) {
-        
+
         ListNode l1 = new ListNode(1);
         l1.next = new ListNode(3);
         l1.next.next = new ListNode(5);
         print(l1);
 
-       ListNode l2 = new ListNode(2);
+        ListNode l2 = new ListNode(2);
         l2.next = new ListNode(4);
         l2.next.next = new ListNode(6);
         print(l2);
 
         ListNode result = addTwoNumbers(l1, l2);
         print(result);
-        
-        
+
+        ListNode rotate = rotateList(result, 43);
+        print(rotate);
     }
 }
