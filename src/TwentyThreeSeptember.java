253f16af2638417cfb37171ee17091f1c549ba27
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-23 13:58:04 +0530

    Code of September

--- src/TwentyThreeSeptember.java
+++ src/TwentyThreeSeptember.java
@@ -57,11 +57,42 @@ public class TwentyThreeSeptember {
         return -1;
     }
 
+    public static int searchInRotatedArray(int[] nums, int target){
+        int start = 0, end = nums.length-1;
+
+        while(start <= end){
+            int mid = start + (end - start)/2;
+
+            if(nums[mid] == target){
+                return mid;
+            }else if(nums[start] <= nums[mid]){
+                if(nums[start] <= target && target <= nums[mid]){
+                    end = mid - 1;
+                }else{
+                    start = mid + 1;
+                }
+            }else{
+                if(nums[mid] <= target && target <= nums[end]){
+                    start = mid + 1;
+                }else {
+                    end = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+
     public static void main(String[] args) {
+
+        int[] nums = {4,5,6,7,0,1,2};
 
         System.out.println(mySqrt(8));
 
         System.out.println(guessNumber(45));
+
+        System.out.println(searchInRotatedArray(nums, 0));
+
+
 
     }
 }
