746c696b3d1caafeb5aad3b94eaabf0233d810a4
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-23 14:06:19 +0530

    Code of September

--- src/TwentyThreeSeptember.java
+++ src/TwentyThreeSeptember.java
@@ -57,24 +57,24 @@ public class TwentyThreeSeptember {
         return -1;
     }
 
-    public static int searchInRotatedArray(int[] nums, int target){
-        int start = 0, end = nums.length-1;
+    public static int searchInRotatedArray(int[] nums, int target) {
+        int start = 0, end = nums.length - 1;
 
-        while(start <= end){
-            int mid = start + (end - start)/2;
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
 
-            if(nums[mid] == target){
+            if (nums[mid] == target) {
                 return mid;
-            }else if(nums[start] <= nums[mid]){
-                if(nums[start] <= target && target <= nums[mid]){
+            } else if (nums[start] <= nums[mid]) {
+                if (nums[start] <= target && target <= nums[mid]) {
                     end = mid - 1;
-                }else{
+                } else {
                     start = mid + 1;
                 }
-            }else{
-                if(nums[mid] <= target && target <= nums[end]){
+            } else {
+                if (nums[mid] <= target && target <= nums[end]) {
                     start = mid + 1;
-                }else {
+                } else {
                     end = mid - 1;
                 }
             }
@@ -82,9 +82,28 @@ public class TwentyThreeSeptember {
         return -1;
     }
 
+    public static int findMinInRotatedArray(int[] nums) {
+        int start = 0, end = nums.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (nums[start] > nums[mid] && nums[mid] < nums[end]) {
+                return mid;
+            }
+
+            if (nums[mid] > nums[end]) {
+                start = mid + 1;
+            } else {
+                end = mid - 1;
+            }
+        }
+        return nums[start];
+    }
+
     public static void main(String[] args) {
 
-        int[] nums = {4,5,6,7,0,1,2};
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
 
         System.out.println(mySqrt(8));
 
@@ -92,6 +111,7 @@ public class TwentyThreeSeptember {
 
         System.out.println(searchInRotatedArray(nums, 0));
 
+        System.out.println(findMinInRotatedArray(nums));
 
 
     }
