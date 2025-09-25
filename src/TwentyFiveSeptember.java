a0e482e12d3c36ba22a25218757f8bbfae24b369
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-25 12:35:45 +0530

    Code of September

--- src/TwentyFiveSeptember.java
+++ src/TwentyFiveSeptember.java
@@ -1,9 +1,9 @@
 public class TwentyFiveSeptember {
-    public static void sortColors(int[] colour){
-        for(int i=0; i<colour.length-1; i++){
+    public static void sortColors(int[] colour) {
+        for (int i = 0; i < colour.length - 1; i++) {
             int min = i;
-            for(int j=i+1; j<colour.length; j++){
-                if(colour[min] > colour[j]){
+            for (int j = i + 1; j < colour.length; j++) {
+                if (colour[min] > colour[j]) {
                     min = j;
                 }
             }
@@ -12,14 +12,47 @@ public class TwentyFiveSeptember {
             colour[i] = temp;
         }
     }
+
+    public static int heightChecker(int[] heights) {
+        int[] expectedHeight = new int[heights.length];
+        for (int i = 0; i < expectedHeight.length; i++) {
+            expectedHeight[i] = heights[i];
+        }
+
+        int count = 0;
+        for (int i = 0; i < expectedHeight.length - 1; i++) {
+            int minheight = i;
+            for (int j = i + 1; j < expectedHeight.length; j++) {
+                if (expectedHeight[minheight] > expectedHeight[j]) {
+                    minheight = j;
+                }
+            }
+            int temp = expectedHeight[minheight];
+            expectedHeight[minheight] = expectedHeight[i];
+            expectedHeight[i] = temp;
+        }
+
+        for (int i = 0; i < expectedHeight.length; i++) {
+            if (expectedHeight[i] != heights[i])
+                count++;
+        }
+        return count;
+    }
+
+
     public static void main(String[] args) {
-        int[] nums = {2,0,2,1,1,0};
+        int[] nums = {2, 0, 2, 1, 1, 0};
 
         sortColors(nums);
+
+        int[] height = {5, 1, 2, 3, 4};
 
         for (int num : nums) {
             System.out.print(num + "==>");
         }
         System.out.println("null");
+
+        System.out.println(heightChecker(height));
+
     }
 }
