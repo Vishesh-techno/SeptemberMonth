9f1df61e31d3f58d7d45947708e64bea7beaf7b7
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-25 14:00:36 +0530

    Code of September

--- src/Sorting.java
+++ src/Sorting.java
@@ -23,6 +23,17 @@ public class Sorting {
             arr[i] = temp;
         }
     }
+    public static void insertionSort(int[] nums){
+        for(int i=1; i<nums.length-1; i++){
+            int curr = nums[i];
+            int prev = i-1;
+            while(prev >= 0 && nums[prev] > curr){
+                nums[prev + 1] = nums[prev];
+                prev--;
+            }
+            nums[prev + 1] = curr;
+        }
+    }
     public static void printArray(int[] arr){
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
@@ -32,7 +43,7 @@ public class Sorting {
     public static void main(String[] args) {
         int[] arr = {5,4,1,3,2,6};
 
-        selectionSort(arr);
+        insertionSort(arr);
         printArray(arr);
     }
 }
