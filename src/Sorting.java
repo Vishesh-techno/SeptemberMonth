ad1791ac01899c8ded9eb2c3156804b14194cb09
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-26 15:35:27 +0530

    Code of September

--- src/Sorting.java
+++ src/Sorting.java
@@ -1,20 +1,21 @@
 public class Sorting {
-    public static void bubbleSort(int[] arr){
-        for(int turn=0; turn<arr.length-1; turn++){
-            for(int j=0; j<arr.length-1-turn; j++){
-                if(arr[j] > arr[j+1]){
+    public static void bubbleSort(int[] arr) {
+        for (int turn = 0; turn < arr.length - 1; turn++) {
+            for (int j = 0; j < arr.length - 1 - turn; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
     }
-    public static void selectionSort(int[] arr){
-        for(int i=0; i<arr.length-2; i++){
+
+    public static void selectionSort(int[] arr) {
+        for (int i = 0; i < arr.length - 2; i++) {
             int minpos = i;
-            for(int j=i+1; j<arr.length-1; j++){
-                if(arr[minpos] > arr[j]){
+            for (int j = i + 1; j < arr.length - 1; j++) {
+                if (arr[minpos] > arr[j]) {
                     minpos = j;
                 }
             }
@@ -23,27 +24,64 @@ public class Sorting {
             arr[i] = temp;
         }
     }
-    public static void insertionSort(int[] nums){
-        for(int i=1; i<nums.length-1; i++){
+
+    public static void insertionSort(int[] nums) {
+        for (int i = 1; i < nums.length - 1; i++) {
             int curr = nums[i];
-            int prev = i-1;
-            while(prev >= 0 && nums[prev] > curr){
+            int prev = i - 1;
+            while (prev >= 0 && nums[prev] > curr) {
                 nums[prev + 1] = nums[prev];
                 prev--;
             }
             nums[prev + 1] = curr;
         }
     }
-    public static void printArray(int[] arr){
+
+    public static void countingSort(int[] nums) {
+        int largest = Integer.MIN_VALUE;
+
+        for (int i = 0; i < nums.length; i++) {
+            largest = Math.max(nums[i], largest);
+        }
+
+        int[] count = new int[largest + 1];
+        for (int i = 0; i < nums.length; i++) {
+            count[nums[i]]++;
+        }
+
+        int j = 0;
+        for (int i = 0; i < count.length; i++) {
+            while (count[i] > 0) {
+                nums[j] = i;
+                j++;
+                count[i]--;
+            }
+        }
+    }
+
+    public static void printArray(int[] arr) {
         for (int i = 0; i < arr.length; i++) {
             System.out.print(arr[i] + " ");
         }
         System.out.println();
     }
-    public static void main(String[] args) {
-        int[] arr = {5,4,1,3,2,6};
 
-        insertionSort(arr);
-        printArray(arr);
+    public static void main(String[] args) {
+        int[] arr1 = {5, 4, 1, 3, 2, 6};
+        int[] arr2 = {5, 4, 1, 3, 2, 6};
+        int[] arr3 = {5, 4, 1, 3, 2, 6};
+        int[] arr4 = {5, 4, 1, 3, 2, 6};
+
+        insertionSort(arr1);
+        printArray(arr1);
+
+        bubbleSort(arr2);
+        printArray(arr2);
+
+        selectionSort(arr3);
+        printArray(arr3);
+
+        countingSort(arr4);
+        printArray(arr4);
     }
 }
