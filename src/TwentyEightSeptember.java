c2eb5b805a2a021b17181b383815c5545e9f36a3
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-28 16:12:17 +0530

    Code of September

--- src/TwentyEightSeptember.java
+++ src/TwentyEightSeptember.java
@@ -14,16 +14,33 @@ public class TwentyEightSeptember {
 //        return new String(ch);
 
         String result = "";
-        for(int i=str.length()-1; i > 0; i--){
-            result =  result + str.charAt(i);
+        for (int i = str.length() - 1; i > 0; i--) {
+            result = result + str.charAt(i);
         }
 
         return result;
+    }
+
+    public static boolean isPalindrome(String str) {
+        char[] ch = str.toCharArray();
+        int left = 0;
+        int right = str.length() - 1;
+
+        while (left <= right) {
+            if (ch[left] != ch[right]) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 
     public static void main(String[] args) {
         String str = "Interview";
 
         System.out.println(reverseString(str));
+
+        System.out.println(isPalindrome(str));
     }
 }
