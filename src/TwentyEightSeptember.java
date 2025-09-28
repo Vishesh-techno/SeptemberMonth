7c1b93b3ae99f9023a7df1013c2a7a0bded0d5ae
Vishesh-techno <sonivishesh099@gmail.com>
2025-09-28 16:08:13 +0530

    Code of September

--- src/TwentyEightSeptember.java
+++ src/TwentyEightSeptember.java
@@ -1,17 +1,24 @@
 public class TwentyEightSeptember {
     public static String reverseString(String str) {
-        char[] ch = str.toCharArray();
-        int left = 0;
-        int right = ch.length - 1;
+//        char[] ch = str.toCharArray();
+//        int left = 0;
+//        int right = ch.length - 1;
+//
+//        while (left <= right) {
+//            char temp = ch[right];
+//            ch[right] = ch[left];
+//            ch[left] = temp;
+//            left++;
+//            right--;
+//        }
+//        return new String(ch);
 
-        while (left <= right) {
-            char temp = ch[right];
-            ch[right] = ch[left];
-            ch[left] = temp;
-            left++;
-            right--;
+        String result = "";
+        for(int i=str.length()-1; i > 0; i--){
+            result =  result + str.charAt(i);
         }
-        return new String(ch);
+
+        return result;
     }
 
     public static void main(String[] args) {
